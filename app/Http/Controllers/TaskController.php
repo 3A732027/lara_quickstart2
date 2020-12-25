2d96e85853de2a3252b729ffbee40d857f27ed7c
Pai Yen Chang <ken1100327@gmail.com>
2020-12-25 18:29:15 +0800

    edit TaskController 的 destroy function  (刪除任務  無授權）

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -98,7 +98,7 @@ class TaskController extends Controller
      */
     public function destroy(Request $request, Task $task)
     {
-        $this->authorize('destroy', $task);
+        //$this->authorize('destroy', $task);
 
         $task->delete();
 
